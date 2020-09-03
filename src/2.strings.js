a3258bd0c85cb1550de9bcab2b21522b6226bcdc
smithpcpuc <smithpcp@mail.uc.edu>
2020-09-03 17:07:50 -0400

    finished test splitString

--- src/2.strings.js
+++ src/2.strings.js
@@ -23,7 +23,7 @@ function capitalize(str) {
 // splitString: should divide a string into substrings and return an array
 // Example: "Jane,Doe,21"  =>  [ "Jane", "Doe", "21" ]
 function splitString(str, splitAt = `,`) {
-  return ;
+  return str.split(',');
 }
 module.exports = {
   reverseString,
